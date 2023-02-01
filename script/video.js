78de903de7e907480c0a76cbde647814126a284e
chakkun1121 <chakkun1121haruki@gmail.com>
2023-02-01 11:44:49 +0900

    名前自動決定をキャンセル

--- script/video.js
+++ script/video.js
@@ -1,39 +1,48 @@
 let nowFacingMode;
 let canUseEnvironmentCamera = true;
 window.onload = function () {
-  cameraRequest()
+  cameraRequest();
   setTimeout(function () {
-    document.querySelector('header').style.top = "-100px";
-  }, 5000)
-}
+    document.querySelector("header").style.top = "-100px";
+  }, 5000);
+};
 function cameraRequest(facingMode = "user") {
-  nowFacingMode = facingMode
-  navigator.mediaDevices.getUserMedia({
-    video: {
-      width: 1920, height: 1080, facingMode: { exact: facingMode }
-    },
-    audio: false
-  }).then(stream => {
-    video.srcObject = stream;
-    video.play()
-  }).catch(e => {
-    console.log(e)
-    alert((typeof e) == String ? 'カメラが使用できません。' : "フロントカメラ以外は使用できません")
-    if ((typeof e) != String) {
-      canUseEnvironmentCamera = false
-    }
-    return e;
-  })
+  nowFacingMode = facingMode;
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        width: 1920,
+        height: 1080,
+        facingMode: { exact: facingMode },
+      },
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((e) => {
+      console.log(e);
+      alert(
+        typeof e == String
+          ? "カメラが使用できません。"
+          : "フロントカメラ以外は使用できません"
+      );
+      if (typeof e != String) {
+        canUseEnvironmentCamera = false;
+      }
+      return e;
+    });
 }
 function takePicture() {
-  canvas.width = video.videoWidth
-  canvas.height = video.videoHeight
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-  const dataURL = canvas.toDataURL()
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  const dataURL = canvas.toDataURL();
   console.log(canvas.toDataURL());
-  downloadDataURL(dataURL)
+  downloadDataURL(dataURL);
 }
-function downloadDataURL(url, title = "picture") {
+function downloadDataURL(url, title = "") {
   const a = document.createElement("a");
   document.body.appendChild(a);
   a.download = title;
@@ -43,9 +52,9 @@ function downloadDataURL(url, title = "picture") {
 }
 function changeFacingMode() {
   if (canUseEnvironmentCamera) {
-    cameraRequest(nowFacingMode == "environment" ? "user" : "environment")
+    cameraRequest(nowFacingMode == "environment" ? "user" : "environment");
     return;
   } else {
-    alert('カメラの切り替えはできません。')
+    alert("カメラの切り替えはできません。");
   }
 }
